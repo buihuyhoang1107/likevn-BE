11e9152ddfbcf4bc88093a58ce29f53a122f7641
buihuyhoang <buihuyhoang9a3@gmail.com>
2025-12-19 19:14:25 +0700

    fix: set default string length to 191 for MySQL utf8mb4 compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        //
+        // Fix for MySQL "Specified key was too long" error
+        // When using utf8mb4, MySQL has a limit of 1000 bytes for index keys
+        // Setting default string length to 191 ensures indexes work correctly
+        Schema::defaultStringLength(191);
     }
 }
 
